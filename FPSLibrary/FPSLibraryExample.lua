1f8fbd5093e82670bcc84b5bfb0c3eb497a6789c
CITY512 <124460799+CITY512@users.noreply.github.com>
2024-10-27 14:07:30 +0000

    Update FPSLibraryExample.lua

--- FPSLibrary/FPSLibraryExample.lua
+++ FPSLibrary/FPSLibraryExample.lua
@@ -1,7 +1,7 @@
 local cloneref = cloneref or function(o) return o end
 local ConfigsDropdown
 getgenv().FPSLibraryProtectGui = true -- Place this above the loadstring
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/CITY512/UI-Libs/refs/heads/main/FPSLibrary/FPSLibrary.lua", true))()
+local Library = require(123286401328821)
 ------------------------------------------------------------------------------------------
 function UpdateConfigsDropdown()
 	ConfigsDropdown.Options = Library:ListConfigurationFiles()
